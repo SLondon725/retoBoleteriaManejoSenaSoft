a9b31be598ee21b5c31b12f1b6280d1e4ad0f387
SLONDON725 <decriloba204@gmail.com>
2025-10-22 11:47:19 -0500

    Enviando peticion de registro de eventos

--- Frontend/js/modulos/eventos.js
+++ Frontend/js/modulos/eventos.js
@@ -1,9 +1,12 @@
 let formulario = document.getElementById("formulario_evento");
 let nombreEvento = document.getElementById("nombre_evento");
 let descripcionEvento = document.getElementById("descripcion_evento");
-let fechaInicioEvento = document.getElementById("fecha_evento");
+let fechaInicioEvento = document.getElementById("fecha_inicio_evento");
+let fechaFinEvento = document.getElementById("fecha_fin_evento");
 let horaInicioEvento = document.getElementById("horaI_evento");
 let horaFinEvento = document.getElementById("horaF_evento");
+let lugarRealizacion = document.getElementById("lugarRealizacion");
+let idMunicipio = document.getElementById("idMunicipio");
 
 
 formulario.addEventListener("submit", (e) => {
@@ -13,24 +16,59 @@ formulario.addEventListener("submit", (e) => {
         nombre: nombreEvento.value,
         descripcion: descripcionEvento.value,
         fechaI: fechaInicioEvento.value,
+        fechaF: fechaFinEvento.value,
         horaI: horaInicioEvento.value,
-        horaF: horaFinEvento.value
+        horaF: horaFinEvento.value,
+        lugarR: lugarRealizacion.value,
+        idMuni: idMunicipio.value
     }
 
-    registrar(evento);
+    registrarEvento(evento);
     console.log(evento)
 });
 
-const registrar = async (evento) =>{
+let registrarEvento = async() =>{
+    const nombre =  nombreEvento.value;
+    const descripcion = descripcionEvento.value;
+    const fechaI =  fechaInicioEvento.value;
+    const fechaF =  fechaFinEvento.value;
+    const horaI =  horaInicioEvento.value;
+    const horaF = horaFinEvento.value;
+    const lugarR = lugarRealizacion.value;
+    const idMuni = idMunicipio.value;
 
-    const res = await fetch("http://localhost:8080/eventos", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(evento)
-    }) 
+    if(nombre == "" || descripcion == "" || fechaI == "" || fechaF == "" || horaI == "" || horaF == "" || lugarR == "" || idMuni == ""){
+        alert("Hay campos vacios, por favor ingrese todos los datos solicitados");
+        return;         
+    }
 
-    return res.json();
+    try{
+
+        const peticion = await fetch("http://localhost:3000/manejo_boleteria/eventos", 
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                nombre,
+                descripcion,
+                fechaI,
+                horaI,
+                horaF,
+                fechaF,
+                lugarR,
+                idMuni
+            })      
+        });
+
+        if (!peticion.ok) throw new Error("Error al registrar el evento");
+
+        const data = await peticion.json();
+        console.log(data.data);
+        alert("Evento registrado exitosamente! ID: "+data.data.id);   
+    }
+    catch(error){
+        console.error(error);
+    }
 }
-
